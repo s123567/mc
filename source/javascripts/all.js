2bf00f15e10b25210a3014b35d3825f414f16396
Mathieu Chiron <mathieu_chiron@yahoo.com>
2016-10-26 16:37:56 +0200

    added basic layout for homepage, philo, temoignage pages. Added google font

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,22 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function() {
+  
+  $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 380) {
+      $('#nav_bar').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 381) {
+      $('#nav_bar').removeClass('navbar-fixed');
+    }
+  });
+
+  
+  
+});
